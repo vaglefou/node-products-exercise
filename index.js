c2f88d1a4d424157a7f2be2662985d5055f63bbb
vagfou <vagfou@protonmail.com>
2024-09-20 20:54:54 +0300

    exercise partially done

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = 3000
+
+mongoose.connect(process.env.MONGODB_URI).then(
+    () => {console.log(`Connection to MongoDB established`) , err => console.log(`Failed to connect to MongoDB`)}
+)
+
+
+app.get('./users')
+
+const user = require('./routes/user.routes')
+app.use('/api/user', user)
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
---
+++ models/user.model.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+let productSchema = new Schema({
+    product: {type: String},
+    cost: {type: Number},
+    quantity: {type: Number, required: true},
+    date: {type: Date, default: Date.now}
+})
+
+let addressSchema = new Schema({
+    area: {type: String},
+    road: {type: String}
+}, {_id: false})
+
+let phoneSchema = new Schema({
+    type: {type: String},
+    number: {type: String}
+}, {id: false})
+
+let userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required field'],
+        max: 100,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String, 
+        required: [true, 'Password is required field'],
+        max: 16
+    },
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        max: 100
+    },
+    surname: {
+        type: String,
+        required: [true, 'Surname is required'],
+        max: 100
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        max: 100,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            "Email address is not valid",
+          ],
+    },
+    address: addressSchema, 
+    phone: {type: [phoneSchema], null: true}, 
+    products: {type: [productSchema], null: true}
+}, {
+    collection: 'users',
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema)
